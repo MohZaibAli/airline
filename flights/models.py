60c511f713c03025d911461ee8a756dc177cd662
MohZaib-Ali <68590303+MohZaib-Ali@users.noreply.github.com>
2020-08-24 16:35:20 +0500

    first commit

---
+++ flights/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+only_alphabetic = RegexValidator(r"^[a-z, A-Z]*$", 'Only Alphabetic Characters (A-Z) Are Allowed.')
+
+# Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3,validators=([only_alphabetic]))
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+
+class Flight(models.Model):
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id} : {self.origin} to {self.destination}"
+
+    def is_valid_flight(self):
+        return self.origin != self.destination and self.duration >= 0
+
+
+class Passenger(models.Model):
+    First_Name = models.CharField(max_length=64)
+    Last_Name = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.First_Name} {self.Last_Name}"
---
+++ users/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+# Create your views here.
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    else:
+        return render(request, "users/user.html", {
+            "message": "Logged In..."
+        })
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid Credentials..."
+            })
+    return render(request, "users/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": "Successfully Logged Out..."
+    })
